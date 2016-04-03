d7b7e639bc907fe15bd944dd7bc5272cb0f20bd0
Rabbanir <rnr254@nyu.edu>
2016-04-03 10:11:32 -0400

    baby android

--- app/src/main/java/com/example/saurabh/makeorbreak/Main2Activity.java
+++ app/src/main/java/com/example/saurabh/makeorbreak/Main2Activity.java
@@ -1,18 +1,36 @@
 package com.example.saurabh.makeorbreak;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.sql.Array;
+import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
 
     TextView momentoftruth;
-    String apidata;
+    String userapidata;
+    String token = "No requests!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +41,35 @@ public class Main2Activity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
-            apidata = extras.getString("Response");
+            userapidata = extras.getString("Response");
         }
+        
+        momentoftruth = (TextView) findViewById(R.id.data);
+        momentoftruth.setText(token);
 
-        momentoftruth = (TextView)findViewById(R.id.data);
-        momentoftruth.setText("Response is: " + apidata);
     }
 
+
+    public void setToken (String t) {
+
+        momentoftruth.setText(t);
+    }
     public void addClaim(View v) {
-        Intent i= new Intent(Main2Activity.this, Main3Activity.class);
+        Intent i = new Intent(Main2Activity.this, Main3Activity.class);
         startActivity(i);
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
