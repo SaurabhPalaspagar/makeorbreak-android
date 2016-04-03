f0d737a2f62cdc46447a7a093cd35f7475fe7f5e
Rabbanir <rnr254@nyu.edu>
2016-04-03 05:38:41 -0400

    claim submission updated

--- app/src/main/java/com/example/saurabh/makeorbreak/Main3Activity.java
+++ app/src/main/java/com/example/saurabh/makeorbreak/Main3Activity.java
@@ -1,13 +1,20 @@
 package com.example.saurabh.makeorbreak;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Main3Activity extends AppCompatActivity {
+
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +23,32 @@ public class Main3Activity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        image = (ImageView) findViewById(R.id.photo);
+
+    }
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public void openCam(View v) {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            BitmapDrawable ob = new BitmapDrawable(getResources(), imageBitmap);
+            image.setImageDrawable(ob);
+        }
     }
 
+    public void submitClaim(View v){
+        //send data to our API
+
+        //redirect to feed
+        Intent i= new Intent(Main3Activity.this, Main2Activity.class);
+        startActivity(i);
+    }
 }
